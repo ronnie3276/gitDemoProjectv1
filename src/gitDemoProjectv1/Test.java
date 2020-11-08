ed74936ba6f6c57025699f78220928a7586db3b7
ema <ronikirsh@gmail.com>
2020-11-08 21:00:12 +0200

    first commit

---
+++ src/gitDemoProjectv1/Test.java
@@ -0,0 +1,15 @@
+package gitDemoProjectv1;
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String hangry = "yes";
+		
+		if (hangry == "yes") 
+			System.out.println("eat something... ");
+		else {
+			System.out.println("Do work...");
+		}
+
+	}
+}
